c657d5c9b61942da9896c8fcec6c5e69829efa7b
Michael Yatrakos <myatrakos@gmail.com>
2018-03-28 19:01:55 +0200

    Created file structure with package.json for npm install

--- client/README.md
+++ client/README.md
@@ -1,5 +1,1 @@
-# CarPool
-
-Make sure you run npm install in both the main app directory and the client directory.
-
 To start the server and the client open your terminal in the client folder and run the command: "npm start".
---
+++ client/src/js/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../res/images/logo.svg';
+import '../css/App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Carpool App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
