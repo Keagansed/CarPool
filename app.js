b4f46ab85a36aa2d80c6cd19493852ed6bd2dc29
MyronOuyang <myronouyang@gmail.com>
2018-03-30 12:26:19 +0200

    added routes and mongoose connecter

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose'); //============== Connect to mongodb ============
 
 var indexRouter = require('./routes/index');
+let signUpRouter = require('./routes/api/signup.js');
+let signInRouter = require('./routes/api/signin.js');
+let verifyRouter = require('./routes/api/verify.js');
+let logoutRouter = require('./routes/api/logout.js');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/carpool'); //========== Define db ================
+mongoose.connection.on('open', function() {
+	console.log('Mongoose connected');
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,6 +28,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/account/signup', signUpRouter)
+app.use('/api/account/signin', signInRouter)
+app.use('/api/account/verify', verifyRouter)
+app.use('/api/account/logout', logoutRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
