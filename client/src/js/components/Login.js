dc10a41ef84b34aac5177a7f5f65d5d759e260c1
Michael Yatrakos <myatrakos@gmail.com>
2018-04-04 14:27:11 +0200

    Moved <Login.js> to components folder

--- client/src/js/components/Login.js
+++ client/src/js/components/Login.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import '../css/App.css';
+import '../../css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
-import { getFromStorage } from './utils/localStorage.js'
-import "./utils/loginQuery.js";
+import { getFromStorage } from '../utils/localStorage.js'
+import "../utils/loginQuery.js";
 
 
 class Login extends Component {
