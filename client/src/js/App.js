556df0cc031a1c46f599a597cba4e0e43ee2599b
MyronOuyang <myronouyang@gmail.com>
2018-04-12 23:35:01 +0200

    fixed vouching

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import logo from '../res/images/logo.svg';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import '../css/App.css';
+
+
 import Login from "./components/Login"
+
+import Profile from './components/Profile';
+import Vouching from './components/Vouching';
 
 class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-        < Login/>
-      </div>
-    );
-  }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       < Login/>
+  //     </div>
+  //   );
+  // }
+
+  render(){
+		return(
+			<BrowserRouter>
+				<Switch>
+					<Route path={"/"} exact component={Login}/>
+					<Route path={"/profile/:_id"} exact component={Profile}/>
+					<Route path={"/vouching/:_id"} exact component={Vouching}/>
+				</Switch>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
