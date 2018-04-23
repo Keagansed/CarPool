7fc2c46c1fcb32dc46c88351fe757a75666edace
MyronOuyang <myronouyang@gmail.com>
2018-04-23 18:45:48 +0200

    removed jquery

--- client/src/js/utils/loginQuery.js
+++ client/src/js/utils/loginQuery.js
@@ -1,82 +1,154 @@
 import $ from 'jquery';
-import { 
-  setInStorage, 
-  getFromStorage
-  } from './localStorage.js'
+import { setInStorage, getFromStorage } from './localStorage.js'
 
-$(document).ready(()=>{
+export const test=()=> {
+	alert("fuckyouplzwork");
+}
 
-    $(document).on("submit","#signUpSubmit",(event)=>{
-        event.preventDefault();
-        fetch('/api/account/signup',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                firstName:$("#signUpfname").val(),
-                lastName:$("#signUplname").val(),
-                email:$("#signUpemail").val(),
-                id:$("#signUpid").val(),
-                password:$("#signUppass").val()
-            })
+export const signUpSubmitFunc = (event)=>{
+	event.preventDefault();
+    fetch('/api/account/signup',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            firstName:$("#signUpfname").val(),
+            lastName:$("#signUplname").val(),
+            email:$("#signUpemail").val(),
+            id:$("#signUpid").val(),
+            password:$("#signUppass").val()
         })
-        .then(res=>res.json())
-        .catch(error => console.error('Error:', error))
-        .then(json=>{
-            console.log('json',json); //========== Probably remove ===============
-            if(json.success){
-                alert("Successfully signed up!!");
-            }else{
-                alert(json.message);
-            }
-        })
-    });
+    })
+    .then(res=>res.json())
+    .catch(error => console.error('Error:', error))
+    .then(json=>{
+        // console.log('json',json); //========== Probably remove ===============
+        if(json.success){
+            alert("Successfully signed up!!");
+        }else{
+            alert(json.message);
+        }
+    })
+}
 
-     $(document).on("submit","#signInSubmit",(event)=>{
-        event.preventDefault();
-        fetch('/api/account/signin',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                email:$("#signInemail").val(),
-                password:$("#signInpass").val()
-            })
+export const signInSubmitFunc = (event)=>{
+	event.preventDefault();
+    fetch('/api/account/signin',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            email:$("#signInemail").val(),
+            password:$("#signInpass").val()
         })
-        .then(res=>res.json())
-        .catch(error => console.error('Error:', error))
-        .then(json=>{
-            if(json.success)
-            {
-                console.log('json',json); //========== Probably remove ===============
-                setInStorage('sessionKey',{token:json.token});
-                window.location.reload();
-            }else{
-                alert(json.message);
-            }
+    })
+    .then(res=>res.json())
+    .catch(error => console.error('Error:', error))
+    .then(json=>{
+        if(json.success)
+        {
             console.log('json',json); //========== Probably remove ===============
             setInStorage('sessionKey',{token:json.token});
             window.location.reload();
-        }) 
-    });
-
-    $(document).on("click","#logOutSubmit",()=>{
-
-        const obj = getFromStorage('sessionKey');
-        if(obj && obj.token){
-          //verify token
-          const { token } = obj;
-          fetch('/api/account/logout?token='+token)
-           .then(res => res.json())
-           .then(json => {
-                console.log('json',json); //========== Probably remove ===============
-                window.location = window.location.herf + "./../../";
-           });
+        }else{
+            alert(json.message);
         }
+        // console.log('json',json); //========== Probably remove ===============
+        setInStorage('sessionKey',{token:json.token});
+        window.location.reload();
+    }) 
+}
 
-    });
+export const logOutSubmitFunc = (event)=>{
+	const obj = getFromStorage('sessionKey');
+    if(obj && obj.token){
+      //verify token
+      const { token } = obj;
+      fetch('/api/account/logout?token='+token)
+       .then(res => res.json())
+       .then(json => {
+            // console.log('json',json); //========== Probably remove ===============
+            window.location = window.location.herf + "./../../";
+       });
+    }
+}
 
-});
+
+// $(document).ready(()=>{
+
+//     $(document).on("submit","#signUpSubmit",(event)=>{
+//         event.preventDefault();
+//         fetch('/api/account/signup',{
+//             method:'POST',
+//             headers:{
+//                 'Content-Type':'application/json'
+//             },
+//             body:JSON.stringify({
+//                 firstName:$("#signUpfname").val(),
+//                 lastName:$("#signUplname").val(),
+//                 email:$("#signUpemail").val(),
+//                 id:$("#signUpid").val(),
+//                 password:$("#signUppass").val()
+//             })
+//         })
+//         .then(res=>res.json())
+//         .catch(error => console.error('Error:', error))
+//         .then(json=>{
+//             console.log('json',json); //========== Probably remove ===============
+//             if(json.success){
+//                 alert("Successfully signed up!!");
+//             }else{
+//                 alert(json.message);
+//             }
+//         })
+//     });
+
+//      $(document).on("submit","#signInSubmit",(event)=>{
+//         event.preventDefault();
+//         fetch('/api/account/signin',{
+//             method:'POST',
+//             headers:{
+//                 'Content-Type':'application/json'
+//             },
+//             body:JSON.stringify({
+//                 email:$("#signInemail").val(),
+//                 password:$("#signInpass").val()
+//             })
+//         })
+//         .then(res=>res.json())
+//         .catch(error => console.error('Error:', error))
+//         .then(json=>{
+//             if(json.success)
+//             {
+//                 console.log('json',json); //========== Probably remove ===============
+//                 setInStorage('sessionKey',{token:json.token});
+//                 window.location.reload();
+//             }else{
+//                 alert(json.message);
+//             }
+//             console.log('json',json); //========== Probably remove ===============
+//             setInStorage('sessionKey',{token:json.token});
+//             window.location.reload();
+//         }) 
+//     });
+
+//     $(document).on("click","#logOutSubmit",()=>{
+
+//         const obj = getFromStorage('sessionKey');
+//         if(obj && obj.token){
+//           //verify token
+//           const { token } = obj;
+//           fetch('/api/account/logout?token='+token)
+//            .then(res => res.json())
+//            .then(json => {
+//                 console.log('json',json); //========== Probably remove ===============
+//                 window.location = window.location.herf + "./../../";
+//            });
+//         }
+
+//     });
+
+// });
 
