514376a998d05e93e21032b3308b8bfc414d363c
VernFrancis <vernon672@gmail.com>
2018-04-27 21:34:08 +0200

    removed jquery from search

--- client/src/js/components/Search.js
+++ client/src/js/components/Search.js
@@ -1,40 +1,58 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
-import "../utils/searchQuery.js";
+import { searchUsers } from "../utils/searchQuery.js";
 import '../../css/App.css';
 
 class Search extends Component {
 	goPro(){
 		alert("fuckyeah");
 	}
+
+	constructor()
+	{
+		super();
+		this.state = {
+			searchResults:""
+		};
+	}
+
+	handleChange = (event) =>
+	{
+		searchUsers(event.target.value).then(res => {
+			this.setState({searchResults : res});
+		});
+	}
+
+	createMarkup = () => {
+		return {__html: this.state.searchResults};
+	}
+
+	// handleClick = (event) => {
+	// 	viewProfile(event.target.getAttribute('data-id'));
+	// }
 	
 	render(){
 		
 		return(
-
+			
             <div className="center_container search_center_container">
-				{/* <div className="bubble"> */}
                     <div className="row no-padding-margin search-container">
 			            <div className="col-md-8 offset-md-2 no-padding">
 			            	<form id="searchUsers">
 				                <div className="row">
 				                    <div className="col-md-12 col-12 no-padding-margin">
 				                        <div className="input-group c-search">
-				                            <input type="text" className="form-control" id="user-search" autoComplete="off" placeholder="Search"/>
+				                            <input type="text" className="form-control" id="user-search" onChange={this.handleChange} autoComplete="off" placeholder="Search"/>
 				                        </div>
 				                    </div>
-				                   {/* <div className="col-md-2">
-				                    	<button type="submit" className="btn btn-info" >Search</button>								
-				                    </div>*/}
 				                </div>
 				            </form>
 							<ul className="" id="user-list">			{/*results injected into this class*/}
-			                    
+			                     <div dangerouslySetInnerHTML={this.createMarkup()} /> 
 							</ul>			                		                
 			            </div>
 			        </div>
-				{/* </div> */}
 			</div>
 		);
 	}
--- client/src/js/utils/searchQuery.js
+++ client/src/js/utils/searchQuery.js
@@ -3,52 +3,51 @@ import $ from 'jquery';
 $(document).ready(()=>{
 	$(document).on("click", ".name",function(e){
 		window.location = window.location.href + "./../" + $(this).attr("data-id");
-	});
-	
-     $(document).on("input cut","#searchUsers",(event) => {
-        event.preventDefault();
-        fetch('/api/account/getUser',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                name:$("#user-search").val(),
-            })
-        })
-        .then(res=>res.json())
-        .catch(error => console.error('Error:', error))
-        .then(json=>{
-
-            if (json.length > 0){
-
-              //~ let profilePic; 
-              var i;
-              var html = "";
-              for (i = 0; i < json.length; ++i) {
-
-                //~ profilePic = "../api/account/getImage?filename=" + json[i].profilePic;
-
-                html += "<div class=\"row searchItem\">";
-                // html += "<img src=\"" + profilePic + "\" class=\"col-md-3 col-4 profilePic\" alt=\"Profile Pic\" /> ";
-                html += "<div class=\"name \" data-id=\"" + json[i]._id + "\">" + json[i].firstName +" " + json[i].lastName + "</div></div>";
-                // html += "<div class=\"name col-md-4 col-8 offset-md-2 offset-sm-0\">" + json[i].firstName +" " + json[i].lastName + "</div></div>"; 
-
-                //~ html += "<li class=\"list-group-item\"><div class=\"col-xs-4\">";
-                //~ html += "<img src=\"" + profilePic + "\" class=\"img-responsive img-circle\" alt=\"Profile Pic\" /> ";
-                //~ html += "<span class=\"name\" data-id=\"" + json[i]._id + "\">" + json[i].firstName +" " + json[i].lastName + "</span><br/></div></li>"; 
-
-              }
-
-              $('#user-list').html(html);
-            }else{
-            	if ($("#user-search").val() === ""){
-            		$('#user-list').html("");
-            	}else{
-              		$('#user-list').html("<div id=\"noUser\">No Such User</div>");
-              	}
-            }
-        });
     });
 });
+
+// export const viewProfile = (profileID) => {
+//     window.location = window.location.href + "./../" + profileID;
+// }
+
+	
+export const searchUsers = (_name) => {
+    let results = "";
+
+    return fetch('/api/account/getUser',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            name:_name,
+        })
+    })
+    .then(res=>res.json())
+    .catch(error => console.error('Error:', error))
+    .then(json=>{
+
+        if (json.length > 0){
+            //~ let profilePic; 
+            var i;
+            results = "";
+
+            for (i = 0; i < json.length; ++i) {
+                //~ profilePic = "../api/account/getImage?filename=" + json[i].profilePic;
+                results += "<div class=\"row searchItem\">";
+                // results += "<img src=\"" + profilePic + "\" class=\"col-md-3 col-4 profilePic\" alt=\"Profile Pic\" /> ";
+                results += "<div class=\"name\" data-id=\"" + json[i]._id + "\">" + json[i].firstName +" " + json[i].lastName + "</div></div>";
+            }
+        }else{
+            if (_name === ""){
+                results = "";
+            }else{
+                results = "<div id=\"noUser\">User Not Found</div>";
+            }
+        }
+        return results;
+    });
+}
+
+
 
