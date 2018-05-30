347c0bc9c8a3e2e157f942da25bba07a161d6831
MyronOuyang <myronouyang@gmail.com>
2018-05-30 15:03:45 +0200

    Google Autocomplete and Map interface Complete

--- client/src/js/components/homepage/routePage/GoogleAuto.js
+++ client/src/js/components/homepage/routePage/GoogleAuto.js
@@ -1,3 +1,5 @@
+/* Googles autocomplete input box */
+
 import React from 'react'
 import { observer } from "mobx-react";
 import RoutesStore from '../../../stores/RoutesStore';
@@ -19,12 +21,11 @@ import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-au
         .then(results => getLatLng(results[0]))
         .then(latLng => {
             console.log("success",latLng);
-            if(this.props.placeholder==='Origin'){
+            if(this.props.placeholder==='Origin'){ 
                 RoutesStore.setOrigin(latLng);
             }else{
                 RoutesStore.setdestination(latLng);
             }
-            // alert(RoutesStore.origin.lat);
         })
         .catch(error => console.error('Error', error))
     }
@@ -32,34 +33,36 @@ import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-au
     render() {
         return (
             <PlacesAutocomplete
-            value={this.state.address}
-            onChange={this.handleChange}
-            onSelect={this.handleSelect}
+                value={this.state.address}
+                onChange={this.handleChange}
+                onSelect={this.handleSelect}
             >
-            {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-                <div>
-                    <input
+                {
+                    ({ getInputProps, suggestions, getSuggestionItemProps }) => (
+                    <div>
+                        <input
                         {...getInputProps({
                             placeholder: this.props.placeholder,
                             className: 'location-search-input' //=========== Class to change the shape and size of input box
                         })}
-                    />
-                    <div className="autocomplete-dropdown-container">
-                        {suggestions.map(suggestion => {
-                            const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                            // inline style for demonstration purpose
-                            const style = suggestion.active
-                            ? { backgroundColor: '#fafafa', cursor: 'pointer', color:'#000' }
-                            : { backgroundColor: '#ffffff', cursor: 'pointer', color:'#000' };
-                            return (
-                                <div {...getSuggestionItemProps(suggestion, { className, style })}>
-                                <span>{suggestion.description}</span>
-                                </div>
-                            )
-                        })}
+                        />
+                        <div className="autocomplete-dropdown-container">
+                            {suggestions.map(suggestion => {
+                                const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                                // inline style for demonstration purpose
+                                const style = suggestion.active
+                                ? { backgroundColor: '#fafafa', cursor: 'pointer', color:'#000' }
+                                : { backgroundColor: '#ffffff', cursor: 'pointer', color:'#000' };
+                                return (
+                                    <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                                    <span>{suggestion.description}</span>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
-                </div>
-            )}
+                    )
+                }
             </PlacesAutocomplete>
         );
     }
--- client/src/js/components/homepage/routePage/MapWrapper.js
+++ client/src/js/components/homepage/routePage/MapWrapper.js
@@ -1,3 +1,5 @@
+/* Wrapper class around GoogleMapComponent (High Order Component) to allow for easy rerender of component when state variable changes */
+
 import React, { Component } from 'react';
 import RoutesStore from '../../../stores/RoutesStore';
 import { observer } from "mobx-react";
@@ -6,26 +8,20 @@ import GoogleMapComponent from './GoogleMap';
 
 @observer class MapWrapper extends Component{
     render(){
-        let GoogleMap = ({coords}) => (
-            <div>
-                <GoogleMapComponent oLat={coords.olat} oLng={coords.olng} dLat={coords.dlat} dLng={coords.dlng} />
-            </div>
+        let GoogleMap = ({coords}) => ( 
+            <GoogleMapComponent oLat={coords.olat} oLng={coords.olng} dLat={coords.dlat} dLng={coords.dlng} />
         );
-
+        
         let coords = {
             olat:RoutesStore.origin.lat,
             olng:RoutesStore.origin.lng,
             dlat:RoutesStore.destination.lat,
             dlng:RoutesStore.destination.lng
         };
-
+        
         return(
-            <div>
-                <GoogleMap coords={coords}/>
-            </div>
-        );
-        
-        
+            <GoogleMap coords={coords}/>
+        ); 
     }
 }
 
