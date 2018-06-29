62e6ff6a67f93b4646202103a43a240b98192cda
Marcus Bornman <marcus.bornman@gmail.com>
2018-06-29 17:48:46 +0200

    Added temporary modals for carpool or user match creation

--- client/src/js/components/home/routePage/matchesPage/Matches.js
+++ client/src/js/components/home/routePage/matchesPage/Matches.js
@@ -5,6 +5,23 @@ import UserMatch from './UserMatch';
 import CarpoolMatch from './CarpoolMatch';
 
 @observer class Matches extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            userID: '',
+            carpoolID: ''
+        }
+    }
+
+    handleUserIDChange(e){
+        this.setState({userID: e.target.value})
+    }
+
+    handleCarpoolIDChange(e){
+        this.setState({carpoolID: e.target.value})
+    }
+
     render(){
         // const { token } = this.props.store;
         
@@ -13,6 +30,38 @@ import CarpoolMatch from './CarpoolMatch';
                 {/* dummy static matches */}
                 <UserMatch />
                 <CarpoolMatch/>
+
+                {/* TEMPORARY >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+                <button id="addUserMatch" className="btn btn-primary margin-top" type="submit" data-toggle="modal" data-target="#userMatchModal">New User Match</button>
+                <div className="modal fade" id="userMatchModal">
+                    <div className="modal-dialog">
+                        <div className="modal-content bubble-more-visible">
+                            <form id="userMatchSubmit">
+                                <div className="form-group">
+                                    <label htmlFor="userID">User ID</label>
+                                    <input type="text" className="form-control" id="userID" onChange={this.handleUserIDChange.bind(this)} />
+                                </div>
+                                <button className="btn btn-secondary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <button id="addCarpoolMatch" className="btn btn-primary margin-top" type="submit" data-toggle="modal" data-target="#carpoolMatchModal">New Carpool Match</button>
+                <div className="modal fade" id="carpoolMatchModal">
+                    <div className="modal-dialog">
+                        <div className="modal-content bubble-more-visible">
+                            <form id="carpoolMatchSubmit">
+                                <div className="form-group">
+                                    <label htmlFor="userID">Carpool ID</label>
+                                    <input type="text" className="form-control" id="userID" onChange={this.handleCarpoolIDChange.bind(this)} />
+                                </div>
+                                <button className="btn btn-secondary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                {/* TEMPORARY <<<<<<<<<<<<<<<<<<<<<<<<<<<<< */}
             </div>
         );
     }
