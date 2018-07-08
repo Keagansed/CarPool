8158ccdb897abfc9f0967f4794c760e6a7576b61
Leonardo Ianigro <u16036094@tuks.co.za>
2018-07-08 19:24:49 +0200

    bug fix

--- client/src/js/components/home/carpoolPage/Carpools.js
+++ client/src/js/components/home/carpoolPage/Carpools.js
@@ -41,7 +41,7 @@ class Carpools extends Component {
             groupChats:[],
             loading: true,
             colours:{0:false,1:false,2:false,3:false,4:false,5:false,6:false},
-            offers:{},
+            offers:[],
         };
 
         this.addChat = this.addChat.bind(this);
