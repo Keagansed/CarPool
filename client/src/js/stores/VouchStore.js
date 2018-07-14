98ecfefbd79bce36aa95b1e05bdbe8a922302ef9
keagansed <keagansed@gmail.com>
2018-07-14 16:20:46 +0200

    User has to enter their thoughts to submit a vouch for someone

--- client/src/js/stores/VouchStore.js
+++ client/src/js/stores/VouchStore.js
@@ -6,24 +6,26 @@ import {
 
 class vouchStore {
     @action submitVouch(tripID, idFor, rating, reviewBody){
-        const obj = getFromStorage('sessionKey');
-        fetch('/api/account/submitVouch',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                tripID:tripID,
-                idBy:obj.token,
-                idFor:idFor,
-                rating:rating,
-                date:new Date(),
-                reviewBody:reviewBody
-            })
-        }).then(res=>res.json())
-            .catch(error => console.error('Error:', error))
-            .then(json=>{
-            });
+        if(reviewBody!== undefined){
+            const obj = getFromStorage('sessionKey');
+            fetch('/api/account/submitVouch',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    tripID:tripID,
+                    idBy:obj.token,
+                    idFor:idFor,
+                    rating:rating,
+                    date:new Date(),
+                    reviewBody:reviewBody
+                })
+            }).then(res=>res.json())
+                .catch(error => console.error('Error:', error))
+                .then(json=>{
+                });
+        }
     }
 }
 
