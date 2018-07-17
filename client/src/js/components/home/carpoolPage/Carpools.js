fafbdb71ec8ca3452152cf21d6be9bc387648ae3
keagansed <keagansed@gmail.com>
2018-07-17 17:58:57 +0200

    Fixed Carpools.js error

--- client/src/js/components/home/carpoolPage/Carpools.js
+++ client/src/js/components/home/carpoolPage/Carpools.js
@@ -93,65 +93,68 @@ class Carpools extends Component {
                     </div>
                     {
                         this.state.groupChats.map((groupChat) => {
-                            for (let user in this.state.groupChats[groupChat.id].users)
-                            {
-                                if(user === getFromStorage('sessionKey').token)
-                                    verifyUser = true;
-                            }
-                            if(verifyUser) {
-                                let usersArray = [];
-                                let users = app.database().ref().child('groupChats/'+groupChat.id+"/users");
-                                users.on('child_added', snap =>{
-                                    usersArray[snap.key] = snap.val();
-                                });
-
-                                let messagesArray = [];
-                                let messages = app.database().ref().child('groupChats/'+groupChat.id+"/messages");
-                                messages.on('child_added', snap =>{
-                                    messagesArray[snap.key] = snap.val();
-                                });
-
-                                let newMessageCount = 0;
-                                for (let message in messagesArray)
-                                {
-                                    let lastRefresh = JSON.parse(usersArray[getFromStorage('sessionKey').token].lastRefresh);
-                                    let messageDate = JSON.parse(messagesArray[message].dateTime);
-                                    if(messageDate > lastRefresh)
-                                        newMessageCount++;
+                            try{
+                                for (let user in this.state.groupChats[groupChat.id].users) {
+                                    if (user === getFromStorage('sessionKey').token)
+                                        verifyUser = true;
                                 }
-                                let messageString = "Messages";
-                                if(newMessageCount === 1)
-                                    messageString = "Message";
-                                return (
-                                    <div key={Math.random()}>
-                                        <Link to={`/HomePage/Chat/`+groupChat.id+'/'+this.state.groupChats[groupChat.id].name}>
-                                            <div className="container-fluid bg-purple bordbot-2px-white">
-                                                <div className="row txt-white padver-10px">
-                                                    <div className="col-9">
-                                                        <div className="col-12">
-                                                            <h5>{this.state.groupChats[groupChat.id].name}</h5>
+                                if (verifyUser) {
+                                    let usersArray = [];
+                                    let users = app.database().ref().child('groupChats/' + groupChat.id + "/users");
+                                    users.on('child_added', snap => {
+                                        usersArray[snap.key] = snap.val();
+                                    });
+
+                                    let messagesArray = [];
+                                    let messages = app.database().ref().child('groupChats/' + groupChat.id + "/messages");
+                                    messages.on('child_added', snap => {
+                                        messagesArray[snap.key] = snap.val();
+                                    });
+
+                                    let newMessageCount = 0;
+                                    for (let message in messagesArray) {
+                                        let lastRefresh = JSON.parse(usersArray[getFromStorage('sessionKey').token].lastRefresh);
+                                        let messageDate = JSON.parse(messagesArray[message].dateTime);
+                                        if (messageDate > lastRefresh)
+                                            newMessageCount++;
+                                    }
+                                    let messageString = "Messages";
+                                    if (newMessageCount === 1)
+                                        messageString = "Message";
+                                    return (
+                                        <div key={Math.random()}>
+                                            <Link
+                                                to={`/HomePage/Chat/` + groupChat.id + '/' + this.state.groupChats[groupChat.id].name}>
+                                                <div className="container-fluid bg-purple bordbot-2px-white">
+                                                    <div className="row txt-white padver-10px">
+                                                        <div className="col-9">
+                                                            <div className="col-12">
+                                                                <h5>{this.state.groupChats[groupChat.id].name}</h5>
+                                                            </div>
+                                                            <div className="col-12">
+                                                                {newMessageCount} New {messageString}
+                                                            </div>
                                                         </div>
-                                                        <div className="col-12">
-                                                            {newMessageCount} New {messageString}
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-3 vertical-right">
-                                                        <div className="col-12">
-                                                            <h5><i className="fa fa-chevron-circle-right"></i></h5>
-                                                        </div>
-                                                        <div className="col-12">
-                                                            {/* Empty for now */}
+                                                        <div className="col-3 vertical-right">
+                                                            <div className="col-12">
+                                                                <h5><i className="fa fa-chevron-circle-right"></i></h5>
+                                                            </div>
+                                                            <div className="col-12">
+                                                                {/* Empty for now */}
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-                                )
+                                            </Link>
+                                        </div>
+                                    )
+                                }
+                                else {
+                                    return (<div key={Math.random()}></div>);
+                                }
                             }
-                            else
-                            {
-                                return (<div key={Math.random()}></div>);
+                            catch (e){
+
                             }
                         })
                     }
