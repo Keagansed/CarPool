6a7c972329e152de9e2d3f6a5ca12cc76254ee92
Vernon <vernon672@gmail.com>
2018-07-19 20:16:08 +0200

    fixed profile store tests

--- client/src/test/stores/ProfileStore.test.js
+++ client/src/test/stores/ProfileStore.test.js
@@ -1,9 +1,6 @@
 import ProfileStore from '../../js/stores/ProfileStore';
 
 describe ('Profile Store test functions', () => {
-    // it ('', () => {
-
-    // });
     let data, values;
 
     it ('initializes variables correctly', () => {
@@ -19,6 +16,66 @@ describe ('Profile Store test functions', () => {
             secLvl : ProfileStore.secLvl,
             profileFound : ProfileStore.profileFound,
             token : ProfileStore.token
+        }
+
+        expect(data).toEqual(values);
+    });
+
+    it ('toggle to vouch tab', () => {
+        values = {
+            vouchTab : true,
+            trustTab : false
+        };
+        
+        ProfileStore.toggleToVouch();
+
+        data = {
+            vouchTab : ProfileStore.vouchTab,
+            trustTab : ProfileStore.trustTab
+        };
+
+        expect(data).toEqual(values);
+    });
+
+    it ('toggle to trust tab', () => {
+        values = {
+            vouchTab : false,
+            trustTab : true
+        };
+
+        ProfileStore.toggleToTrust();
+
+        data = {
+            vouchTab : ProfileStore.vouchTab,
+            trustTab : ProfileStore.trustTab
+        };
+
+        expect(data).toEqual(values);
+    });
+
+    it ('sets edit variables', () => {
+        ProfileStore.user = {
+            firstName : 'testy',
+            lastName : 'McTestFace',
+            email : 'tester@test.com',
+            id : '9311251585081',
+            profilePic : ''
+        }
+
+        ProfileStore.setEdit();
+
+        values = {
+            eFName : 'testy',
+            eLName : 'McTestFace',
+            eEmail : 'tester@test.com',
+            eID : '9311251585081'
+        }
+
+        data = {
+            eFName : ProfileStore.eFName,
+            eLName : ProfileStore.eLName,
+            eEmail : ProfileStore.eEmail,
+            eID : ProfileStore.eID
         }
 
         expect(data).toEqual(values);
