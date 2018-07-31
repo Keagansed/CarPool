5f6be5647896eecbb7a9d44dc926071fc480a016
Marcus Bornman <marcus.bornman@gmail.com>
2018-07-31 10:57:57 +0200

    Home NavTabs.js coding standards done

--- client/src/js/components/navigation/NavTabs.js
+++ client/src/js/components/navigation/NavTabs.js
@@ -1,22 +1,39 @@
-// import { Link } from 'react-router-dom';
-import { observer } from 'mobx-react';
+// File Type: Component
+
 import React, { Component } from 'react';
-import RoutesStore from "../../stores/RoutesStore";
+import { observer } from 'mobx-react';
 
 import NewRouteModal from '../route/NewRouteModal';
+import RoutesStore from "../../stores/RoutesStore";
 
-@observer class NavTabs extends Component{
+/*
+* The purpose of this NavTabs class is to provide a tabular menu that is available on the Home page.
+* This tabbed menu provides access to the "Routes", "Carpools", and "Trips" tabs on the Home page2.
+*/
+@observer class NavTabs extends Component {
 
-    constructor(){
+    /*
+    * The purpose of the constructor method is to instantiate fields to relevant values. In this case
+    * the routeTabActive field is set to true because the "Routes" tab is active when the Home page is first
+    * loaded.
+    */
+    constructor() {
         super()
 
         this.state = {
+            //routeTabActive represents the status of the "Routes" tab.
             routeTabActive: "active",
+            //carpoolTabActive represents the status of the "Carpools" tab.
             carpoolTabActive: "",
+            //tripTabActive represents the status of the "Trips" tab.
             tripTabActive: "",    
         }
     }
 
+    /*
+    * The purpose of the handleRouteToggle method is to set the "Routes" tab to active. The method
+    * will also deactivate the other tabs.
+    */
     handleRouteToggle = (event) => {
         event.preventDefault();
 
@@ -25,9 +42,13 @@ import NewRouteModal from '../route/NewRouteModal';
             routeTabActive: "active",
             carpoolTabActive: "",
             tripTabActive: "", 
-        })
+        });
     }
 
+    /*
+    * The purpose of the handleCarPoolToggle method is to set the "Carpools" tab to active. The method
+    * will also deactivate the other tabs.
+    */
     handleCarPoolToggle = (event) => {
         event.preventDefault();
 
@@ -36,9 +57,13 @@ import NewRouteModal from '../route/NewRouteModal';
             routeTabActive: "",
             carpoolTabActive: "active",
             tripTabActive: "", 
-        })
+        });
     }
 
+    /*
+    * The purpose of the handleTripToggle method is to set the "Trips" tab to active. The method
+    * will also deactivate the other tabs.
+    */
     handleTripToggle = (event) => {
         event.preventDefault();
 
@@ -47,25 +72,41 @@ import NewRouteModal from '../route/NewRouteModal';
             routeTabActive: "",
             carpoolTabActive: "",
             tripTabActive: "active", 
-        })
+        });
     }
 
-    render(){
+    /*
+    * The purpose of the render method is to enable the rendering of this component.
+    * It returns react elements using JSX.
+    */
+    render() {
         return(
             <div className="fixed-top container-fluid height-50px bg-aqua">
                 <div className="row font-20px height-100p">
                     <div className="col-10 pad-0">
-                        <button className={"btnTab col-4 btn height-100p bg-trans txt-purple fw-bold brad-0 font-20px " + this.state.routeTabActive} onClick={this.handleRouteToggle}>
+                        <button 
+                            className = {"btnTab col-4 btn height-100p bg-trans txt-purple fw-bold brad-0 font-20px " + this.state.routeTabActive} 
+                            onClick = {this.handleRouteToggle}
+                        >
                             Routes
                         </button>
-                        <button className={"btnTab col-4 btn height-100p bg-trans txt-purple fw-bold brad-0 font-20px " + this.state.carpoolTabActive} onClick={this.handleCarPoolToggle}>
+                        <button 
+                            className = {"btnTab col-4 btn height-100p bg-trans txt-purple fw-bold brad-0 font-20px " + this.state.carpoolTabActive} 
+                            onClick = {this.handleCarPoolToggle}
+                        >
                             Carpools
                         </button>
-                        <button className={"btnTab col-4 btn height-100p bg-trans txt-purple fw-bold brad-0 font-20px " + this.state.tripTabActive} onClick={this.handleTripToggle}>
+                        <button 
+                            className = {"btnTab col-4 btn height-100p bg-trans txt-purple fw-bold brad-0 font-20px " + this.state.tripTabActive} 
+                            onClick = {this.handleTripToggle}
+                        >
                             Trips
                         </button>
                     </div>
-                    <NewRouteModal store={RoutesStore} token={this.props.store.token}/>
+                    <NewRouteModal 
+                        store = {RoutesStore} 
+                        token = {this.props.store.token}
+                    />
                 </div>
             </div>
         )
