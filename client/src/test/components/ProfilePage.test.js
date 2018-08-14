46aab28543eb394b0fff387f804c1250d8794999
Vernon <vernon672@gmail.com>
2018-08-14 21:50:46 +0200

    add mockStore to ProfilePage.test

--- client/src/test/components/ProfilePage.test.js
+++ client/src/test/components/ProfilePage.test.js
@@ -2,19 +2,56 @@ import React from 'react';
 import ProfilePage from '../../js/containers/ProfilePage';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
 import ProfileStore from "../../js/stores/ProfileStore";
+
+class MockStore {
+    user = {};
+    secLvl = 0;
+    profileFound = false;
+    token = null;
+    opacity = "opacity-0";
+    firstName = '';
+    lastName = '';
+    email = '';
+    idNum = '';
+    profilePic = '';
+    vouchTab = true;
+    trustTab = false;
+
+    getProfile = (token) => {
+        this.token = token;
+        this.user = {};
+        this.firstName = "Test";
+        this.lastName = "Tester";
+        this.email = "test@email.com";
+        this.opacity = "";
+        this.profilePic = "img";
+        this.secLvl = 5;
+    }
+
+    toggleToVouch = () => {
+        this.vouchTab = true;
+        this.trustTab = false;
+    }
+
+    toggleToTrust = () => {
+        this.trustTab = true;
+        this.vouchTab = false;
+    }
+}
 
 describe('Profile Page Component', () => {
     const match = { params: { _id: '123Foo' } };
-    let container;
+    let container, instance, mockStore;
 
-    beforeEach(() => {
-        container = shallow(<ProfilePage store={ProfileStore} match={match} />);
+    beforeAll(() => {
+        mockStore = new MockStore();
+        container = shallow(<ProfilePage store={mockStore} match={match} />);
+        instance = container.instance;
     });
 
-    it ('captures snapshot', () => {
-        const renderedValue = renderer.create(<ProfilePage store={ProfileStore} match={match} />).toJSON();
+    it('captures snapshot', () => {
+        const renderedValue = renderer.create(<ProfilePage store={mockStore} match={match} />).toJSON();
         expect(renderedValue).toMatchSnapshot();
     });
 
