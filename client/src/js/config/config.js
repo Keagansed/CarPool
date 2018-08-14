438846f76a7f07848fd0bf3e2c68c517a7c9645a
keagansed <keagansed@gmail.com>
2018-08-14 15:29:04 +0200

    Fixed gitignore for config.js

--- client/src/js/config/config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyD1qEOLQIoyANZgqNIEVh-ykTxWhOJJoqI",
-    authDomain: "cos301-carpool.firebaseapp.com",
-    databaseURL: "https://cos301-carpool.firebaseio.com",
-    projectId: "cos301-carpool",
-    storageBucket: "cos301-carpool.appspot.com",
-    messagingSenderId: "937269341053"
-};
