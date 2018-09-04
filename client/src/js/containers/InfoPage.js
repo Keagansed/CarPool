81ba52dfb07e5dfce016c52bbc4d82ae48b96e11
Marcus Bornman <marcus.bornman@gmail.com>
2018-09-04 16:32:13 +0200

    Info Page Framework Added

--- client/src/js/containers/InfoPage.js
+++ client/src/js/containers/InfoPage.js
@@ -3,32 +3,47 @@
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 
-import logo  from "../../css/images/logo.png";
+import logo  from "../../css/images/logo-gif-loop.gif";
 
 /*
 * Purpose: Landing page compenent is the first page the user sees when opening the app
 */
-class LandingPage extends Component {
-    componentWillMount() {
-        LoginStore.setRegistered(false);
-    }
-
+class InfoPage extends Component {
     render() {
         return(
             <div className="vertical-center bg-white">
                 <div className="container-fluid">
                     <div className="row">
+                        <h1 className="txt-center mx-auto">The Iminsys Carpool Platform</h1>
+                    </div>
+                    <div className="row">
                         <img className="img-fluid d-block mx-auto mbottom-2rem" src={logo} alt="carpool_logo" id="logo-256"/> 
                     </div>
                     <div className="row">
-                        <Link to={`/Login`} className="btn btn-primary mx-auto width-15rem brad-2rem mbottom-1rem bg-aqua txt-purple fw-bold" id="btnLogin">
-                            Login
-                        </Link>
-                    </div>
-                    <div className="row">
-                        <Link to={`/Register`} className="btn btn-primary mx-auto width-15rem brad-2rem bg-aqua txt-purple fw-bold" id="btnRegister">
-                            Register
-                        </Link>
+                        <div className="col-12 col-lg-3">
+                            <h3 className="txt-center">Heading One</h3>
+                            <p className="txt-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed magna accumsan, lacinia lectus nec, euismod metus. Fusce laoreet, lectus nec sagittis aliquam, tellus odio elementum nulla, eget rhoncus mi purus a arcu. Sed fermentum erat id purus viverra, at pellentesque dolor posuere. In hac habitasse platea dictumst. Praesent quis magna nec mi tincidunt ullamcorper a a mi. Nulla ornare rutrum leo et mollis. Donec iaculis feugiat orci, vitae sollicitudin turpis ullamcorper varius. Donec at tempus tellus. Aliquam sit amet est et justo porta tempor.
+                            </p>
+                        </div>
+                        <div className="col-12 col-lg-3">
+                            <h3 className="txt-center">Heading One</h3>
+                            <p className="txt-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed magna accumsan, lacinia lectus nec, euismod metus. Fusce laoreet, lectus nec sagittis aliquam, tellus odio elementum nulla, eget rhoncus mi purus a arcu. Sed fermentum erat id purus viverra, at pellentesque dolor posuere. In hac habitasse platea dictumst. Praesent quis magna nec mi tincidunt ullamcorper a a mi. Nulla ornare rutrum leo et mollis. Donec iaculis feugiat orci, vitae sollicitudin turpis ullamcorper varius. Donec at tempus tellus. Aliquam sit amet est et justo porta tempor.
+                            </p>
+                        </div>
+                        <div className="col-12 col-lg-3">
+                            <h3 className="txt-center">Heading One</h3>
+                            <p className="txt-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed magna accumsan, lacinia lectus nec, euismod metus. Fusce laoreet, lectus nec sagittis aliquam, tellus odio elementum nulla, eget rhoncus mi purus a arcu. Sed fermentum erat id purus viverra, at pellentesque dolor posuere. In hac habitasse platea dictumst. Praesent quis magna nec mi tincidunt ullamcorper a a mi. Nulla ornare rutrum leo et mollis. Donec iaculis feugiat orci, vitae sollicitudin turpis ullamcorper varius. Donec at tempus tellus. Aliquam sit amet est et justo porta tempor.
+                            </p>
+                        </div>
+                        <div className="col-12 col-lg-3">
+                            <h3 className="txt-center">Heading One</h3>
+                            <p className="txt-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed magna accumsan, lacinia lectus nec, euismod metus. Fusce laoreet, lectus nec sagittis aliquam, tellus odio elementum nulla, eget rhoncus mi purus a arcu. Sed fermentum erat id purus viverra, at pellentesque dolor posuere. In hac habitasse platea dictumst. Praesent quis magna nec mi tincidunt ullamcorper a a mi. Nulla ornare rutrum leo et mollis. Donec iaculis feugiat orci, vitae sollicitudin turpis ullamcorper varius. Donec at tempus tellus. Aliquam sit amet est et justo porta tempor.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -36,4 +51,4 @@ class LandingPage extends Component {
     }
 }
 
-export default LandingPage;
+export default InfoPage;
