7a57211a861e7be775b07dc53a9f081d9f7c8f49
Leonardo Ianigro <u16036094@tuks.co.za>
2018-09-20 22:50:06 +0200

    config

--- client/src/js/utils/server.js
+++ client/src/js/utils/server.js
@@ -1,9 +1,9 @@
 // local version
-export default "http://localhost:3001"
+// export default "http://localhost:3001"
 // export default "http://192.168.88.58:3001"
 
 // live version
-// export default "https://backend.carpool.iminsys.com"
+export default "https://backend.carpool.iminsys.com"
 
 // debug version
 // export default ""
