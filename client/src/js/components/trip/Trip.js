b062101b535e7839d4021553659c5676385d7f95
Marcus Bornman <marcus.bornman@gmail.com>
2018-10-06 20:32:36 +0200

    Trips tab done

--- client/src/js/components/trip/Trip.js
+++ client/src/js/components/trip/Trip.js
@@ -2,6 +2,13 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import InfoIcon from '@material-ui/icons/ViewList';
+import TripIcon from '@material-ui/icons/NearMe';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
 
 /**
  * Purpose: An interface to allow the user to set up a Trip
@@ -31,29 +38,19 @@ class Trip  extends Component {
         let date = days[dayNameNumber] + " " + day + " " + monthNames[month] + " " + year;
 
         return(
-            <div className="container-fluid bg-purple bordbot-2px-white">
-                <Link to={`/HomePage/Trip/`+this.props.trip._id}>
-                    <div className="row txt-white padver-10px">
-                        <div className="col-9 padright-0">
-                            <div className="col-12 padright-0">
-                                <h5>{this.props.trip.tripName}</h5>
-                            </div>
-                            <div className="col-12 padright-0">
-                                {date} @ {time}
-                            </div>
-                        </div>
-                        <div className="col-3 vertical-right">
-                            <div className="col-12">
-                                <h5><i className="fa fa-chevron-circle-right"></i></h5>
-                            </div>
-                            {/*Average trip rating*/}
-                            {/*<div className="col-12 txt-10px txt-gold">*/}
-                                {/*<i className="fa fa-star"></i> <i className="fa fa-star"></i> <i className="fa fa-star"></i> <i className="fa fa-star"></i> <i className="fa fa-star"></i>*/}
-                            {/*</div>*/}
-                        </div>
-                    </div>
-                </Link>
-            </div>
+            <Link to={`/HomePage/Trip/`+this.props.trip._id} style={{ textDecoration: 'none' }}>
+                <ListItem button>
+                    <Avatar>
+                        <TripIcon />
+                    </Avatar>
+                    <ListItemText primary={this.props.trip.tripName} secondary={date + " @ " + time} />
+                    <ListItemSecondaryAction>
+                        <IconButton aria-label="View Trip Details">
+                            <InfoIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+            </Link>
         );
     }
 }
